6fedff1e2aa6b7ca0d5bb78a52bd13d36ba8ebd1
Chandler <chandlersquires@gmail.com>
2021-12-20 10:47:52 -0600

    remove unnecessary folders

--- algorithms/mean.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def mean(x):
-    return np.mean(x)
-
-
-def median(x):
-    return np.median(x)
--- experiments/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-BASE_FOLDER = os.path.dirname(__file__)
-DATA_FOLDER = os.path.join(BASE_FOLDER, 'data')
-RESULT_FOLDER = os.path.join(BASE_FOLDER, 'results')
-FIGURE_FOLDER = os.path.join(BASE_FOLDER, 'figures')
